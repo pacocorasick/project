443d0960c799ff1f225f2f8d809a2e7b97b60dd9
pacocorasick <cvet.k@leanst.com>
2018-08-31 21:24:37 +0300

    Removed dist and fix

--- dist/js/prettyPhoto.js
+++
@@ -1,7 +0,0 @@
-        $(document).ready(function () {
-            $("a[rel^='prettyPhoto']").prettyPhoto({
-                theme: 'facebook',
-                slideshow: 5000,
-                autoplay_slideshow: false
-            });
-        });
